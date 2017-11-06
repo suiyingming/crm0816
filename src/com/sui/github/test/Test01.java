5f6e202418cead9bffde6ecc370ffdc0feb261ce
yingming <6657714@qq.com>
2017-11-06 20:20:58 +0800

    vcvc

--- src/com/sui/github/test/Test01.java
+++ src/com/sui/github/test/Test01.java
@@ -6,7 +6,8 @@ public class Test01 {
 		System.out.println("hello,github·þÎñÆ÷");
 		System.out.println("ok,github update01");
 		System.out.println("ok,local update01");
-		
+		System.out.println("ok,local update01");
+		System.out.println("cvcvcvcvcvcvcvcvte01");
 	}
 
 }
