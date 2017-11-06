1be6a79f302c65dcfbbc2853669946070000205d
yingming <6657714@qq.com>
2017-11-06 19:59:33 +0800

    local update01

--- src/com/sui/github/test/Test01.java
+++ src/com/sui/github/test/Test01.java
@@ -5,6 +5,8 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello,github·þÎñÆ÷");
 		System.out.println("ok,github update01");
+		System.out.println("ok,local update01");
+		
 	}
 
 }
