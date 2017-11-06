47fc85d4925576d90e938193de02b2370d99c9bd
suiyingming <33385042+suiyingming@users.noreply.github.com>
2017-11-06 19:49:54 +0800

    update01

--- src/com/sui/github/test/Test01.java
+++ src/com/sui/github/test/Test01.java
@@ -3,7 +3,8 @@ package com.sui.github.test;
 public class Test01 {
 	
 	public static void main(String[] args) {
-		System.out.println("hello,github·şÎñÆ÷");
+		System.out.println("hello,githubÂ·Ã¾ÃÃ±Ã†Ã·");
+		System.out.println("ok,github update01");
 	}
 
 }
